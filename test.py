5d2bb6c205f74055478a82d9978d26496888dad3
Matt Clark <clarkmatthew@eucahost-2-185.eucalyptus>
2011-11-08 15:16:11 -0800

    clean up

--- test.py
+++ test.py
@@ -2,6 +2,4 @@ from eutester import eutester
 import time
 if __name__ == '__main__':
     clc_session = eutester( credpath="eucarc-eucalyptus-admin", password="foobar")
-    image
-   
     clc_session.do_exit()
