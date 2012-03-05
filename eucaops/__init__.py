4d04b9f8cd0fc84e7ed87af9d796f692843009be
Matt Clark <clarkmatthew@eucahost-2-185.eucalyptus>
2012-03-05 14:41:23 -0800

    Fix typos in some of error handling

--- eucaops/__init__.py
+++ eucaops/__init__.py
@@ -122,7 +122,7 @@ class Eucaops(Eutester):
         try:
             self.walrus.get_bucket(bucket).get_key(name)
             self.fail("Walrus bucket still exists after delete")
-        except Excetption, e:
+        except Exception, e:
             return
         
     
@@ -555,7 +555,7 @@ class Eucaops(Eutester):
             self.debug("Attemtping to associate " + str(address) + " from " + str(instance))
             address.associate(instance.id)
         except Exception as (errno, strerror):
-            self.critical("Unable to associate address\n" + str(e))
+            self.critical("Unable to associate address\n")
             self.critical( "Exception({0}): {1}".format(errno, strerror))
             return False
         self.debug("Associated IP successfully")
@@ -662,7 +662,7 @@ class Eucaops(Eutester):
         self.debug( "Attempting to run image " + str(image) + " in group " + group)
         reservation = image.run(key_name=keypair,security_groups=[group],instance_type=type, placement=zone, min_count=min, max_count=max)
         if ((len(reservation.instances) < min) or (len(reservation.instances) > max)):
-            self.fail("Reservation:"+str(res.id)+" returned "+str(len(reservation.instances))+" instances, not within min("+str(min)+") and max("+str(max)+" ")
+            self.fail("Reservation:"+str(reservation.id)+" returned "+str(len(reservation.instances))+" instances, not within min("+str(min)+") and max("+str(max)+" ")
             
         self.wait_for_reservation(reservation)
         for instance in reservation.instances:
