fe7585e13d855113a41450d599adaac618c75070
Harold Spencer Jr <hspencer77@gmail.com>
2012-04-25 16:13:11 -0700

    Updated iptables_security_group_test to use unittest; version 0.1.1

--- testcases/unstable/iptables_security_group_test.py
+++ testcases/unstable/iptables_security_group_test.py
@@ -13,174 +13,190 @@ from eucaops import Eucaops
 import argparse
 import random
 import os
+import unittest
 import time
 import string
+import re
+import sys
+
+class Instances(unittest.TestCase):
+    def setUp(self):
+        self.options = options
+
+        ### Make sure --config_file or --credpath is provied
+        self.got_creds = False
+        while self.got_creds == False:      
+            try:
+	        if self.options.config_file:
+            	    self.tester = Eucaops(config_file=self.options.config_file, password=self.options.password)
+	        elif self.options.credpath:
+            	    self.tester = Eucaops(credpath=self.options.credpath, password=self.options.password)
+                else:
+		    print "\tNeed to pass either --credpath or --config_file option. Try --help for more information"	
+		    exit(1)
+            except Exception,e:
+                print str(e) 
+                self.time.sleep(30)
+                continue
+            self.got_creds = True
+
+        self.tester.start_euca_logs()
+
+    def tearDown(self):
+        ### Clean up after running test case
+        self.tester.delete_keypair(self.keypair)
+        os.remove(self.keypath)
+        self.tester.stop_euca_logs()
+        self.tester.save_euca_logs()
+        self.keypath = None
+        self.keypair = None
+        self.tester = None
+        self.image = None
+        self.options = None
+        self.got_creds = None
+        self.time = None
+        self.num_vms = None
+        self.available = None
+        self.security_groups = None
+
+    ### Test Cases ###
+    def iptables_Cruft(self):
+        ### Launch number of instances based upon number of security groups wanting to be tested.
+
+        ## If specific image wants to be tested, use that; if not, use any instance-store backed image.
+        if (self.options.image):
+    	    self.image = self.tester.get_emi(emi=self.options.image)
+        else:
+	    self.image = self.tester.get_emi(root_device_type="instance-store")
+
+        self.keypair = self.tester.add_keypair(self.options.prefix + "-" + str(time.time())) 
+        self.keypath = os.curdir + "/" + self.keypair.name + ".pem"
+
+        ### Identify type of instance to run
+        if self.options.type == "random":
+                self.options.type = random.choice(["m1.small","c1.medium","m1.large","m1.xlarge","c1.xlarge"])
+            
+        ### Identify number of instances to run (i.e. number of security groups)
+        self.num_vms = self.tester.get_available_vms(self.options.type)
+
+	if self.num_vms >= self.options.number:
+            self.available = self.options.number
+        else:
+            self.options.type = "m1.small"
+            avail_vms = self.tester.get_available_vms(self.options.type)
+            if avail_vms < self.options.number:
+                self.tester.fail("Not enough m1.small vm types to run test with minimal of 3 security groups.")
+            else:
+                self.available = self.options.number
+
+        ### Take snapshot of iptables before creating security groups and launching instances.
+        self.pre_iptables = self.tester.sys("iptables-save | grep -v \"#\" | grep -v \"\:PRE\" | grep -v \"\:POST\" | grep -v \"\:INPUT\" | grep -v \"\:FORWARD\" | grep -v \"\:OUT\"")
+        self.assertNotEqual(len(self.pre_iptables), 0, "pre_Iptables_Snapshot failed.")
+            
+        ### Create security group for number of security groups we want to test.
+	self.security_groups = []
+	while self.available > 0:
+            ### Create unique security group and authorize SSH and PING
+            reservation = None
+            sec_group = None
+            sec_group = self.tester.add_group(group_name=self.options.prefix + "-" + str(time.time()))
+            self.assertNotEqual(len(sec_group.name), 0, "Could not create group.")
+            self.assertTrue(self.tester.authorize_group_by_name(group_name=sec_group.name),
+                    "Could not authorize group for SSH")
+            self.assertTrue(self.tester.authorize_group_by_name(group_name=sec_group.name, port=-1, protocol="icmp" ),
+                    "Could not authorize group for PING")
+            self.security_groups.append(sec_group)
+          
+            ### Launch instance for the unique security group 
+            try:
+               reservation = self.tester.run_instance(self.image,keypair=self.keypair.name, group=sec_group.name ,type=self.options.type)  
+            except Exception, e:
+               self.fail("Caught an exception when running the instance: " + str(e))
+
+            for instance in reservation.instances:
+                ### Wait for instance to run; error otherwise
+                self.assertTrue(self.tester.wait_for_reservation(reservation) ,'Instance did not go to running')
+
+                ### Ping the instance
+                ping_result = self.tester.ping(instance.public_dns_name)
+                self.assertTrue(ping_result, "Ping to instance failed.")
+               
+                ### If test is running from Eucalyptus component, access instance and run "uname -a" 
+                if (self.options.config_file):
+                    uname_result = instance.sys("uname -a")
+                    self.assertNotEqual(len(uname_result), 0, "uname -a failed.")
+
+            ### Decrement count of security groups and instances left to create
+	    self.available -= 1 
+
+
+        ### Loop through and terminate instances                
+        ### Grab total number of instances ran from by test case
+        total_reservations = self.tester.ec2.get_all_instances()
+        ### Terminate each instance
+        for reservation in total_reservations:
+            self.assertTrue(self.tester.terminate_instances(reservation), "Failure when terminating instance.")
+
+        ### Loop through and delete security groups                
+        for group in self.security_groups: 
+            self.assertTrue(self.tester.delete_group(group), "Failure when deleting group " + group.name)
+
+        ### Take snapshot of iptables after deleting security groups and launching instances.
+        self.post_iptables = self.tester.sys("iptables-save | grep -v \"#\" | grep -v \"\:PRE\" | grep -v \"\:POST\" | grep -v \"\:INPUT\" | grep -v \"\:FORWARD\" | grep -v \"\:OUT\"")
+        self.assertNotEqual(len(self.post_iptables), 0, "post_Iptables_Snapshot failed.")
+
+        ### Evaluate pre and post iptables outputs to see if there is a difference.
+        self.iptables_diff = self.tester.diff(self.pre_iptables, self.post_iptables)
+        if (len(self.iptables_diff) > 0):
+            print "Diffences between iptables snapshots: " 
+            for diff in self.iptables_diff:
+                print diff
+
+
+def get_options():
+    ### Parse args
+    parser = argparse.ArgumentParser(prog="iptables_security_group_test.py", 
+        version="Test Case [iptables_security_group_test.py] Version 0.1.1",
+        description="Run an iterative test of operations on a cloud to test integrity of iptables \
+            state upon deletion of security groups.  For more information, please refer to \
+            https://github.com/hspencer77/eutester/wiki/iptables_security_group_test")
+    parser.add_argument("-n", "--number", dest="number", type=int,
+        help="Number of security groups to create", default=3)
+    parser.add_argument("-e", "--exit", action="store_true", dest="exit_on_fail",
+        help="Whether or not to stop the script after a failure")
+    parser.add_argument("-i", "--image", dest="image", 
+        help="Specific image to run", default=None)
+    parser.add_argument("-t", "--type",  dest="type",
+        help="Type of instance to launch Default: random", default="random")
+    parser.add_argument( "--prefix", dest="prefix", 
+        help="Prefix to tack on to keypairs", default="iptables-secgrp-test")
+    parser.add_argument("-z", "--zone",  dest="zone",
+        help="AZ to run script against", default="PARTI00")
+    parser.add_argument("-u", "--user",  dest="user",
+        help="User to run script as", default="admin")
+    parser.add_argument("-a", "--account",  dest="account",
+        help="Account to run script as", default="eucalyptus")
+    parser.add_argument("-U", "--username",  dest="username",
+        help="User account on physical CC machine", default="root")
+    parser.add_argument("-P", "--password",  dest="password",
+        help="Password for user account on physical CC machine", default=None)
+    parser.add_argument("--config_file",  dest="config_file",
+        help="Cloud config of AZ", default=None)
+    parser.add_argument("--credpath",  dest="credpath",
+        help="AZ to run script against", default=None)
+    parser.add_argument('unittest_args', nargs='*')
+
+    ## Grab arguments passed via commandline
+    options = parser.parse_args() 
+    sys.argv[1:] = options.unittest_args
+    return options
 
 if __name__ == '__main__':
-    ### Parse args
-    parser = argparse.ArgumentParser(prog="iptables_security_group_test.py", version="Test Case [iptables_security_group_test.py] Version 0.1", description='Run an iterative test of operations on a cloud to test integrity of iptables state upon deletion of security groups.  For more information, please refer to https://github.com/hspencer77/eutester/wiki/iptables_security_group_test')
-    parser.add_argument("-r","--runs", dest="runs", type=int,
-                      help="How many times to run test", default=1) 
-    parser.add_argument("-n", "--number", dest="number", type=int,
-                      help="Number of security groups to create", default=3)
-    parser.add_argument("-e", "--exit", action="store_true", dest="exit_on_fail",
-                      help="Whether or not to stop the script after a failure")
-    parser.add_argument("-i", "--image", dest="image", 
-                      help="Specific image to run", default=None)
-    parser.add_argument("-t", "--type",  dest="type",
-                      help="Type of instance to launch Default: random", default="random")
-    parser.add_argument( "--prefix", dest="prefix", 
-                      help="Prefix to tack on to keypairs", default="iptables-secgrp-test")
-    parser.add_argument("-z", "--zone",  dest="zone",
-                      help="AZ to run script against", default="PARTI00")
-    parser.add_argument("-u", "--user",  dest="user",
-                      help="User to run script as", default="admin")
-    parser.add_argument("-a", "--account",  dest="account",
-                      help="Account to run script as", default="eucalyptus")
-    parser.add_argument("-U", "--username",  dest="username",
-                      help="User account on physical CC machine", default="root")
-    parser.add_argument("-P", "--password",  dest="password",
-                      help="Password for user account on physical CC machine", default=None)
-    parser.add_argument("--config_file",  dest="config_file",
-                      help="Cloud config of AZ", default=None)
-    parser.add_argument("--credpath",  dest="credpath",
-                      help="AZ to run script against", default=None)
-    
-    options = parser.parse_args()
-    ### LOAD OPTIONS INTO LOCAL VARS
-
-    got_creds = False
-    while got_creds == False:      
-        try:
-	    if options.config_file:
-            	tester = Eucaops(config_file=options.config_file, password=options.password, boto_debug=0)
-	    elif options.credpath:
-            	tester = Eucaops(credpath=options.credpath, password=options.password, boto_debug=0)
-            else:
-		print "\tNeed to pass either --credpath or --config_file option. Try --help for more information"	
-		exit(1)
-        except Exception,e:
-            print str(e) 
-            time.sleep(30)
-            continue
-        got_creds = True
-    
-    if (options.image):
-    	image = tester.get_emi(emi=options.image)
-    else:
-	image = tester.get_emi(root_device_type="instance-store")
-
-    pwd =  os.getcwd()
-    try:    
-        fail = 0
-        success = 0
-        keypath = None
-        keypair = None
-        while options.runs > 0:
-            ## identify type of instance to run
-            if options.type == "random":
-                options.type = random.choice(["m1.small","c1.medium","m1.large","m1.xlarge","c1.xlarge"])
-            
-            ## identify number of instances to run (i.e. number of security groups)
-            num_vms = tester.get_available_vms(options.type)
-
-	    if num_vms > options.number:
-		available = options.number
-	    else:
-		options.type = "m1.small"
-		avail_vms = tester.get_available_vms(options.type)
-		if avail_vms < options.number:
-			tester.fail("Not enough m1.small vm types to run test")
-		else:
-            		available = options.number
-
-            ### Create keypair
-            try:
-                keypair = tester.add_keypair(options.prefix + "-" + str(time.time()))
-            except Exception,e :
-                print str(e)
-                continue
-
-            keypath = pwd + "/" + keypair.name + ".pem"
-            
-            ### Create security groups
-	    security_groups = []
-	    while available > 0:
-            	try:
-                	sec_group = tester.add_group(group_name=options.prefix + "-" + str(time.time()))
-            	except Exception,e :
-               	 	print str(e)
-                	continue
-            	### Defaults to authorizing SSH, then run it again for authorizing icmp
-            	try:   
-                	tester.authorize_group(sec_group)
-                	tester.authorize_group(sec_group, port=-1, protocol="icmp" )
-			security_groups.append(sec_group)
-            	except Exception,e :
-                	print str(e)
-                	sec_group.delete()
-                	continue
-            
-            	try:
-                	reservation = tester.run_instance(image,keypair=keypair.name, group=sec_group.name ,type=options.type)
-            	except Exception, e:
-                	tester.fail("Failed launching instance")
-
-            	### Some hypervisors will require a few seconds after "running" state for instance to boot
-            	tester.sleep(15)
-            
-            	### Log into each instance
-            	for instance in reservation.instances:
-                	## Update instance info
-                	instance.update()
-                
-               	if instance.state != "running":
-                    tester.fail("Instance did not go to running state")
-                    available -= 1
-                    continue
-                
-                if instance.public_dns_name == instance.private_ip_address:
-                    tester.fail("Did not get a private IP")
-                    available -= 1
-                    continue
-               
-		available -= 1 
-	    #
-	    # Take snapshot of iptables on CC
-	    #
-
-	    # Loop through and terminate instances                
-	    total_reservations = tester.ec2.get_all_instances()
-	    for reservation in total_reservations:
-            	tester.terminate_instances(reservation)
-
-	    # Loop through and delete security groups                
-	    for group in security_groups: 
-            	tester.delete_group(group)
-
-	    #
-	    # Take snapshot of iptables on CC
-	    #
-
-
-            tester.delete_keypair(keypair)
-	    os.remove(keypath)
-
-            options.runs -= 1
-            if tester.fail_count > 0: 
-                fail += 1
-                tester.fail_log = []
-                if options.exit_on_fail:
-                    tester.fail_count= fail
-                    tester.do_exit()
-            else:
-                success += 1
-            tester.fail_count = 0     
-            tester.running_log = []
-            #tester.tee( "*************************************************************")
-            #tester.tee( "* Ran " + str( fail + success ) + " times")
-            #tester.tee( "* Success: " + str(success) + " Failures: " + str(fail))
-            #tester.tee( "*************************************************************")                      
-    except KeyboardInterrupt:
-        exit(1)
-    
+    options = get_options()
+    tests = ['iptables_Cruft']
+    for test in tests:
+        result = unittest.TextTestRunner(verbosity=2).run(Instances(test)) 
+        if result.wasSuccessful():
+            pass
+        else:
+            exit(1)
