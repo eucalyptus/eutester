e11686f363e8635c67901e3c679d195ab770bc40
Vic Iglesias <viglesiasce@gmail.com>
2012-06-13 22:38:40 -0700

    Use unique prefix for each type of resources

--- eutester/eupopulator.py
+++ eutester/eupopulator.py
@@ -59,7 +59,6 @@ class EuPopulator(object):
             self.defaults.add_section('global')
             partition = self.tester.ec2.get_all_zones()[0].name
             self.defaults.set('global', 'partition', partition)
-            resource_prefix = self.prefix_generator()
             ### Volume 
             self.defaults.add_section('volumes')
             self.defaults.set('volumes', 'count', '2')
@@ -76,12 +75,12 @@ class EuPopulator(object):
             ### Keypair 
             self.defaults.add_section('keypairs')
             self.defaults.set('keypairs', 'count', '2')
-            self.defaults.set('keypairs', 'prefix', "key" + resource_prefix)
+            self.defaults.set('keypairs', 'prefix', self.prefix_generator())
             
             ### Groups 
             self.defaults.add_section('security_groups')
             self.defaults.set('security_groups', 'count', '2')
-            self.defaults.set('security_groups', 'prefix', "sg-" + resource_prefix)
+            self.defaults.set('security_groups', 'prefix', self.prefix_generator())
             self.defaults.set('security_groups', 'port_min', '22')
             self.defaults.set('security_groups', 'port_max', '80')
             self.defaults.set('security_groups', 'protocol', 'tcp')
@@ -90,10 +89,10 @@ class EuPopulator(object):
             ### Buckets
             self.defaults.add_section('buckets')
             self.defaults.set('buckets', 'count', '2')
-            self.defaults.set('buckets', 'prefix', 'bukkit-' + resource_prefix)
+            self.defaults.set('buckets', 'prefix', self.prefix_generator())
             self.defaults.set('buckets', 'add_keys', 'true')
             self.defaults.set('buckets', 'key_count', '2')
-            self.defaults.set('buckets', 'key_prefix', resource_prefix)
+            self.defaults.set('buckets', 'key_prefix', self.prefix_generator())
             self.config = self.defaults
 
         else:
