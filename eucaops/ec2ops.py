52cd98201a191ed8125dcc11579ec7eb4a685ad9
Vic Iglesias <vic.iglesias@eucalyptus.com>
2012-08-01 21:46:34 -0700

    Move eucalyptus specific methods into Eucaops

--- eucaops/ec2ops.py
+++ eucaops/ec2ops.py
@@ -45,12 +45,10 @@ from boto.exception import EC2ResponseError
 from eutester.euinstance import EuInstance
 
 class EC2ops(Eutester):
-    def __init__(self, config_file=None, password=None, keypath=None, credpath=None, aws_access_key_id=None, aws_secret_access_key = None,account="eucalyptus",user="admin", username="root",region=None, clc_ip=None, walrus_ip=None, boto_debug=0):
-        Eutester.__init__(self, config_file=config_file,password=password, keypath=keypath, credpath=credpath, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,account=account, user=user, region=region,  walrus_ip=walrus_ip, clc_ip=clc_ip, boto_debug=boto_debug)
+    def __init__(self, credpath=None, aws_access_key_id=None, aws_secret_access_key = None, username="root",region=None, ec2_ip=None, s3_ip=None, boto_debug=0):
+        Eutester.__init__(self, credpath=credpath, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region=region,  s3_ip=s3_ip, ec2_ip=ec2_ip, boto_debug=boto_debug)
         self.poll_count = 48
         self.username = username
-        if self.hypervisor is "vmware":
-            self.poll_count = 96
         self.test_resources = {}
         self.setup_ec2_resource_trackers()
         
