db2e11670087afcbe8fc1f91f04f4d287e84c3ef
Vic Iglesias <vic.iglesias@eucalyptus.com>
2012-10-16 18:54:32 -0700

    Tweak instance_restore test

--- testcases/cloud_admin/instance_restore.py
+++ testcases/cloud_admin/instance_restore.py
@@ -33,8 +33,13 @@ class InstanceRestore(EutesterTestCase):
         self.private_addressing = False
         zones = self.tester.ec2.get_all_zones()
         self.zone = random.choice(zones).name
-
+        clcs = self.tester.get_component_machines("clc")
+        if len(clcs) is 0:
+            raise Exception("No CLC found")
+        else:
+            self.clc = clcs[0]
         self.cur_time = str(int(time.time()))
+        self.ncs = self.tester.get_component_machines("nc")
 
     def clean_method(self):
         ncs = self.tester.get_component_machines("nc")
@@ -47,14 +52,23 @@ class InstanceRestore(EutesterTestCase):
         self.tester.delete_keypair(self.keypair)
         os.remove(self.keypath)
 
+        ### RESET vmstate properties
+        self.modify_property("cloud.vmstate.instance_timeout","60")
+        self.modify_property("cloud.vmstate.terminated_time","60")
+        for nc in self.ncs:
+            nc.sys("service eucalyptus-nc start")
+
     def restore_logic(self):
+        self.modify_property("cloud.vmstate.instance_timeout","1")
+        self.modify_property("cloud.vmstate.terminated_time","1")
+
         self.reservation = self.tester.run_instance(self.image, keypair=self.keypair.name, group=self.group.name, zone=self.zone)
-        ncs = self.tester.get_component_machines("nc")
-        for nc in ncs:
+
+        for nc in self.ncs:
             nc.sys("service eucalyptus-nc stop")
 
         ### Wait for instance to show up as terminating
-        self.tester.wait_for_reservation(self.reservation, state="terminated", timeout=900)
+        self.tester.wait_for_reservation(self.reservation, state="terminated", timeout=600)
 
         ### Wait for reservation to disappear
         while len(self.tester.get_instances(reservation=self.reservation)) > 0:
@@ -62,12 +76,24 @@ class InstanceRestore(EutesterTestCase):
 
         self.tester.deregister_image(self.image, clear=True)
 
-        for nc in ncs:
+        for nc in self.ncs:
             nc.sys("service eucalyptus-nc start")
 
-        self.tester.wait_for_reservation(self.reservation, state="running", timeout=900)
+        self.tester.wait_for_reservation(self.reservation, state="running", timeout=600)
         for instance in self.reservation.instances:
             instance.sys("uname -r", code=0)
+
+    def modify_property(self, property, value):
+        """
+        Modify a eucalyptus property through the command line euca-modify-property tool
+        property        Property to modify
+        value           Value to set it too
+        """
+        command = "source " + self.tester.credpath + "/eucarc && " + self.tester.eucapath + "/usr/sbin/euca-modify-property -p " + str(property) + "=" + str(value)
+        if self.clc.found(command, property):
+            self.debug("Properly modified property " + property)
+        else:
+            raise Exception("Setting property " + property + " failed")
 
 if __name__ == "__main__":
     testcase = EutesterTestCase()
