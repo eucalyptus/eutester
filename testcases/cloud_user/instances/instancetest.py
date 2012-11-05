627cb465f8e25ff46bf021ecdc7b076bc21983cd
Vic Iglesias <viglesiasce@gmail.com>
2012-11-05 12:36:13 -0800

    When using InstanceBasics as a base class, ensure we can pass extra command line args.

--- testcases/cloud_user/instances/instancetest.py
+++ testcases/cloud_user/instances/instancetest.py
@@ -17,9 +17,11 @@ import random
 
 
 class InstanceBasics(EutesterTestCase):
-    def __init__(self):
+    def __init__(self, extra_args= None):
         self.setuptestcase()
         self.setup_parser()
+        for arg in extra_args:
+            self.parser.add_argument(arg)
         self.get_args()
         # Setup basic eutester object
         self.tester = Eucaops( credpath=self.args.credpath)
