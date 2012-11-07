9492ebf48be5cf51001ccfbcf38ea179df4bd270
Vic Iglesias <viglesiasce@gmail.com>
2012-11-07 15:57:42 -0800

    create_snapshot should only return 1 snap

--- eucaops/ec2ops.py
+++ eucaops/ec2ops.py
@@ -813,8 +813,11 @@ class EC2ops(Eutester):
         :param description: (optional integer) over all time to wait before exiting as failure
         :return: EuSnapshot
         """
-        return self.create_snapshots_from_vol_id(volume_id, count=1, mincount=1, eof=True, wait_on_progress=wait_on_progress, poll_interval=poll_interval, timeout=timeout, description=description)
-        
+        snapshots = self.create_snapshots_from_vol_id(volume_id, count=1, mincount=1, eof=True, wait_on_progress=wait_on_progress, poll_interval=poll_interval, timeout=timeout, description=description)
+        if len(snapshots) == 1:
+            return snapshots[0]
+        else:
+            raise Exception("When creating a single snapshot, multiple or none were returned")
     
     def create_snapshots_from_vol_id(self,volume_id, count=1, mincount=None, eof=True, delay=0, wait_on_progress=20, poll_interval=10, timeout=0, description=""):
         """
