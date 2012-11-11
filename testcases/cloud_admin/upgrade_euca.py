b42f067b836266fdc59ccc7260be3b760b66534e
Vic Iglesias <viglesiasce@gmail.com>
2012-11-11 15:21:44 -0800

    Add back all upgrade steps

--- testcases/cloud_admin/upgrade_euca.py
+++ testcases/cloud_admin/upgrade_euca.py
@@ -9,14 +9,40 @@ class Upgrade(EutesterTestCase):
     def __init__(self, extra_args= None):
         self.setuptestcase()
         self.setup_parser()
-        self.parser.add_argument("--euca-url")
+        self.parser.add_argument("--euca-url",)
         self.parser.add_argument("--enterprise-url")
+        self.parser.add_argument("--branch")
         if extra_args:
             for arg in extra_args:
                 self.parser.add_argument(arg)
         self.get_args()
         # Setup basic eutester object
         self.tester = Eucaops( config_file=self.args.config_file, password=self.args.password, download_creds=False)
+
+        if not self.args.branch and not self.args.euca_url and not self.args.enterprise_url:
+            self.args.branch = self.args.upgrade_to_branch
+
+        ### IF we were passed a branch, fetch the correct repo urls from the repo API
+        if self.args.branch:
+            self.args.euca_url = self.get_repo_url("eucalyptus", self.args.branch)
+            self.args.enterprise_url =self.get_repo_url("internal", self.args.branch)
+
+
+
+    def clean_method(self):
+        pass
+
+    def get_repo_url(self, repo = "eucalyptus", branch = "testing"):
+        import httplib
+        api_host = "packages.release.eucalyptus-systems.com"
+        machine = self.tester.get_component_machines("clc")[0]
+        path="/api/1/genrepo/?distro="+str(machine.distro.name)+"&releasever=6&arch=x86_64&url=repo-euca@git.eucalyptus-systems.com:"+str(repo)+"&ref="+str(branch) + "&allow-old"
+        conn=httplib.HTTPConnection(api_host)
+        conn.request("GET", path)
+        res=conn.getresponse()
+        repo_url = res.read().strip()
+        self.tester.debug("Setting " + repo + " URL to: " + repo_url)
+        return repo_url
 
     def add_euca_repo(self):
         for machine in self.tester.config["machines"]:
@@ -40,12 +66,11 @@ class Upgrade(EutesterTestCase):
                 machine.sys("service eucalyptus-cc start")
 
     def UpgradeAll(self):
-        #self.add_euca_repo()
+        self.add_euca_repo()
         if self.args.enterprise_url:
             self.add_enterprise_repo()
         self.upgrade_packages()
         self.start_components()
-
 
 if __name__ == "__main__":
     testcase = Upgrade()
