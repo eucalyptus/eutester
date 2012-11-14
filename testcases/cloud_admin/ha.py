6a2035d12aafeacaff1c4c4d72eb88c1f73e41bf
Vic Iglesias <viglesiasce@gmail.com>
2012-11-13 23:22:29 -0800

    Add back tests commented out for debug

--- testcases/cloud_admin/ha.py
+++ testcases/cloud_admin/ha.py
@@ -143,7 +143,7 @@ class HAtests(InstanceBasics, BucketTestSuite):
         primary_service = service_aquisition_callback()
         secondary_service = self.tester.service_manager.wait_for_service(primary_service, state="DISABLED")
         self.tester.debug("Primary Service: " + primary_service.machine.hostname + " Secondary Service: " + secondary_service.machine.hostname)
-        primary_service.machine.interrupt_network(600)    
+        primary_service.machine.interrupt_network(300)
         
         if "clc" in primary_service.machine.components:
             self.tester.debug("Switching ec2 connection to host: " +  secondary_service.machine.hostname)
@@ -190,10 +190,10 @@ class HAtests(InstanceBasics, BucketTestSuite):
     def failoverCLC(self):
         self.failoverService(self.servman.get_enabled_clc, self.MetaData)
         self.post_run_checks()
-        #self.failoverReboot(self.servman.get_enabled_clc, self.MetaData)
-        #self.post_run_checks()
-        #self.failoverNetwork(self.servman.get_enabled_clc, self.MetaData)
-        #self.post_run_checks()
+        self.failoverReboot(self.servman.get_enabled_clc, self.MetaData)
+        self.post_run_checks()
+        self.failoverNetwork(self.servman.get_enabled_clc, self.MetaData)
+        self.post_run_checks()
     
     def failoverWalrus(self):
         self.failoverService(self.servman.get_enabled_walrus, self.test_bucket_get_put_delete)
