689446880a5b881d1a8a7f45ee105779c154cf98
Matt Clark <mclark@eucalyptus.com>
2012-11-16 21:36:43 -0800

    Add additional comments, debug, and clear checked_devs for md5 with multiple vols

--- eutester/euinstance.py
+++ eutester/euinstance.py
@@ -542,7 +542,8 @@ class EuInstance(Instance):
         #check to see if there's existing data that we should avoid overwriting 
         if overwrite or ( int(self.sys('head -c 32 '+str(voldev)+' | xargs -0 printf %s | wc -c')[0]) == 0):
             self.random_fill_volume(euvolume, srcdev=srcdev, length=length)
-        self.debug("Volume has existing data, skipping random data fill")
+        else:
+            self.debug("Volume has existing data, skipping random data fill")
         #calculate checksum of euvolume attached device for given length
         md5 = self.md5_attached_euvolume(euvolume, timepergig=timepergig,length=length)
         self.debug("Filled Volume:"+euvolume.id+" dev:"+voldev+" md5:"+md5)
@@ -635,7 +636,7 @@ class EuInstance(Instance):
         
     def get_unsynced_volumes(self,euvol_list=None, md5length=32, timepervol=90,min_polls=2, check_md5=False):
         '''
-        Returns list of volumes which are:
+        Description: Returns list of volumes which are:
         -in a state the cloud believes the vol is no longer attached
         -the attached device has changed, or is not found.
         If all euvols are shown as attached to this instance, and the last known local dev is present and/or a local device is found with matching md5 checksum
@@ -644,11 +645,11 @@ class EuInstance(Instance):
         A subset can be provided in the list argument 'euvol_list'. 
         Returns a list of euvolumes for which a corresponding guest device could not be found, or the cloud no longer believes is attached. 
          
-        euvol_list - optional - euvolume object list. Defaults to all self.attached_vols
-        md5length - optional - defaults to the length given in each euvolume. Used to calc md5 checksum of devices
-        timerpervolume -optional - time to wait for device to appear, per volume before failing
-        min_polls - optional - minimum iterations to check guest devs before failing, despite timeout
-        check_md5 - optional - find devices by md5 comparision. Default is to only perform this check when virtio_blk is in use.
+        :param euvol_list: - optional - euvolume object list. Defaults to all self.attached_vols
+        :param md5length: - optional - defaults to the length given in each euvolume. Used to calc md5 checksum of devices
+        :param timerpervolume: -optional - time to wait for device to appear, per volume before failing
+        :param min_polls: - optional - minimum iterations to check guest devs before failing, despite timeout
+        :param check_md5: - optional - find devices by md5 comparision. Default is to only perform this check when virtio_blk is in use.
         '''
         bad_list = []
         vol_list = []
@@ -661,16 +662,17 @@ class EuInstance(Instance):
             vol_list.append(euvol_list)
         else:
             vol_list = self.attached_vols
-        
-            
+        self.debug("Checking for volumes whos state is not in sync with our instance's test state...")
         for vol in vol_list:
             #first see if the cloud believes this volume is still attached. 
             try: 
                 self.debug("Checking volume:"+str(vol.id))
                 if (vol.attach_data.instance_id == self.id): #verify the cloud status is still attached to this instance
+                    self.debug("Cloud beleives volume:"+str(vol.id)+" is attached to:"+str(self.id)+", check for guest dev...")
                     found = False
                     elapsed = 0 
                     start = time.time()
+                    checked_vdevs = []
                     #loop here for timepervol in case were waiting for a volume to appear in the guest. ie attaching
                     while (not found) and ((elapsed <= timepervol) or (poll_count < min_polls)):
                         try:
@@ -678,6 +680,7 @@ class EuInstance(Instance):
                             #Ugly... :-(
                             #handle virtio and non virtio cases differently (KVM case needs improvement here).
                             if self.virtio_blk or check_md5:
+                                self.debug('Checking any new devs for md5:'+str(vol.md5))
                                 #Do some detective work to see what device name the previously attached volume is using
                                 devlist = self.get_dev_dir()
                                 for vdev in devlist:
