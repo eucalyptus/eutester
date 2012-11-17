e24130432ee8890f32bc8527e94f305793753912
Matt Clark <mclark@eucalyptus.com>
2012-11-16 23:05:26 -0800

    Adjust args for cli or input file w/o cli defaults over writing

--- testcases/cloud_user/ebs/multi_node_churn.py
+++ testcases/cloud_user/ebs/multi_node_churn.py
@@ -37,19 +37,21 @@ if __name__ == "__main__":
     testcase= EutesterTestCase(name='multi_node_churn')    
     testcase.setup_parser(description="Attempts to test and provide info on focused areas related to Eucalyptus EBS related functionality.", 
                           testlist=False)
-    testcase.parser.add_argument('--count', type=int, help='Number of times to run attach/detach churn',default=10)
-    testcase.parser.add_argument('--nodecount', type=int, help='Number of nodes in env',default=2)
-    testcase.parser.add_argument('--fof', dest='fof', help="Freeze test on fail, do not remove or tear down test items",action='store_true', default=False)
-
+    testcase.parser.add_argument('--count', type=int, help='Number of times to run attach/detach churn',default=None)
+    testcase.parser.add_argument('--nodecount', type=int, help='Number of nodes in env',default=None)
+    testcase.parser.add_argument('--fof', dest='fof', help="Freeze test on fail, do not remove or tear down test items",action='store_true', default=None)
     testcase.get_args()
+    count = testcase.args.count or 10
+    fof =  testcase.args.fof if testcase.args.fof is not None else False
+    nodecount = testcase.args.nodecount or 2
     ebstestsuite= testcase.do_with_args(EbsTestSuite)
     if testcase.args.fof:
         testcase.clean_method = lambda: testcase.status("Freeze on fail flag is set, not cleaning!")
     else:
         testcase.clean_method = ebstestsuite.clean_created_resources
     testlist = ebstestsuite.test_multi_node(run=False, 
-                                            count=int(testcase.args.count),
-                                            nodecount=int(testcase.args.nodecount))
+                                            count=int(count),
+                                            nodecount=int(nodecount))
     ret = testcase.run_test_case_list(testlist)
     print "mutli node test exiting:("+str(ret)+")"
     exit(ret)
