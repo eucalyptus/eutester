6cba2a2f91bcfee8b4aa5642e011fd41e4ce7935
Tony Beckham <tony@eucalyptus.com>
2013-01-04 12:59:23 -0800

    add clean method stub to be completed later

--- eucaops/asops.py
+++ eucaops/asops.py
@@ -75,6 +75,10 @@ class ASops(Eutester):
 #        self.setup_ec2_resource_trackers()
         self.key_dir = "./"
 
+    def clean_method(self):
+        ### once needed clean up should be done here
+
+
     def setup_as_connection(self, endpoint=None, aws_access_key_id=None, aws_secret_access_key=None, is_secure=True,host=None ,
                              region=None, path = "/", port = 443,  APIVersion ='2011-01-01', boto_debug=0):
         as_region = RegionInfo()
