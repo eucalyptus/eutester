95c3f731ff0710f24779f1cd729d46c0fa503808
Matt Clark <mclark@eucalyptus.com>
2013-02-06 22:18:49 -0800

    Rely upon remote commands return code for eval

--- testcases/cloud_user/instances/create_bfebs_img_test.py
+++ testcases/cloud_user/instances/create_bfebs_img_test.py
@@ -258,12 +258,7 @@ if __name__ == '__main__':
             pmsg("curl cmd's parsed result:"+result)
         except Exception, e:
             raise Exception("failed to curl image into block dev: "+str(e))
-        #Make sure the curl command did not return error
-        if (re.match('GOOD',result)):
-            pmsg("Our write to volume returned GOOD")
-        else:
-            raise Exception("Our write to volume failed:"+str(result))
-      
+        
         #Get the md5sum of the volume to compare to our previous md5         
         cmd="head -c "+str(fbytes)+" "+attached_block_dev+" | md5sum "
         md5sum2=instance.sys(cmd ,  timeout=timeout)[0]
