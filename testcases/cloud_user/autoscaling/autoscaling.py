ecfbadb9473db2e365736f5f2e9344204c7e9f5a
Tony Beckham <tony.beckham@gmail.com>
2013-02-11 15:03:57 -0800

    hard code this bitch even harder

--- testcases/cloud_user/autoscaling/autoscaling.py
+++ testcases/cloud_user/autoscaling/autoscaling.py
@@ -98,8 +98,11 @@ class AutoScalingBasics(EutesterTestCase):
             This case was developed to exercise creating a new launch configuration
             image_id="ami-0af30663" a us-east image
         """
-        self.tester.create_launch_config(name="test_lc", image_id=str(self.image), key_name=str(self.keypair),
-                                         security_groups=self.group.name)
+        # self.tester.create_launch_config(name="test_lc", image_id=str(self.image), key_name=str(self.keypair),
+        #                                  security_groups=self.group.name)
+
+        self.tester.create_launch_config(name="test_lc", image_id="ami-0af30663", key_name="key2",
+                                         security_groups="default")
 
     def DeleteLaunchConfiguration(self):
         """
