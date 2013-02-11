fd06080c3ce614e786eb492014bb14fe2c004276
Vic Iglesias <viglesiasce@gmail.com>
2013-02-10 20:18:38 -0800

    Setup aws access and secret keys

--- eucaops/iamops.py
+++ eucaops/iamops.py
@@ -36,7 +36,11 @@ import boto
 class IAMops(Eutester):
 
     def __init__(self,credpath=None, endpoint="iam.amazonaws.com", aws_access_key_id=None,aws_secret_access_key=None, is_secure=True, port=443, path='/', boto_debug=0 ):
-        super(IAMops, self).__init__(credpath=credpath, aws_access_key_id=aws_access_key_id ,aws_secret_access_key=aws_secret_access_key)
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.user_id = None
+        self.account_id = None
+        super(IAMops, self).__init__(credpath=credpath)
         self.setup_iam_connection(endpoint=endpoint, aws_access_key_id=self.aws_access_key_id ,aws_secret_access_key=self.aws_secret_access_key, is_secure=is_secure, port=port,path=path, boto_debug=boto_debug )
 
     def setup_iam_connection(self, endpoint="iam.amazonaws.com", aws_access_key_id=None,aws_secret_access_key=None, is_secure=True, port=443, path='/', boto_debug=0 ):
