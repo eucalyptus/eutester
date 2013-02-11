22c135db0dfe27935c59c6da158ffd27d6ddec14
Tony Beckham <tony.beckham@gmail.com>
2013-02-11 15:52:28 -0800

    set endpoint

--- eucaops/asops.py
+++ eucaops/asops.py
@@ -137,7 +137,7 @@ class ASops(Eutester):
             as_connection_args['region'] = as_region
             self.debug("Attempting to create auto scale connection to " + as_region.endpoint + ":" + str(port) + path)
 
-            self.AS = boto.connect_autoscale("AKIAJWTY6ISPYZS4XV5A", "yFXY022/j7eeVGzeLoTBy4cIhd1upXHpve6R0wiy")
+            self.AS = boto.connect_autoscale("AKIAJWTY6ISPYZS4XV5A", "yFXY022/j7eeVGzeLoTBy4cIhd1upXHpve6R0wiy","us-west-1")
 
             # self.AS = boto.ec2.autoscale.AutoScaleConnection("AKIAJWTY6ISPYZS4XV5A", "yFXY022/j7eeVGzeLoTBy4cIhd1upXHpve6R0wiy")
             # self.AS = boto.ec2.autoscale.connect_to_region("us-west-1")
