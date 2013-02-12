68c59f00e779d732ada671e9ae5a9b5179f8b23c
Tony Beckham <tony.beckham@gmail.com>
2013-02-11 23:00:34 -0800

    list AS groups and delete

--- testcases/cloud_user/autoscaling/autoscaling.py
+++ testcases/cloud_user/autoscaling/autoscaling.py
@@ -98,7 +98,7 @@ class AutoScalingBasics(EutesterTestCase):
 
     def cleanAll(self):
         # self.debug("Singled out: " + str(self.tester.describe_as_group(names="AS-3fd01ddf2b491374")[0]))
-        self.tester.delete_as_group(names=["AS-3fd01ddf2b491374"], force=True)
+        self.tester.delete_as_group(names="AS-3fd01ddf2b491374")
         for item in self.tester.describe_as_group():
             self.debug("Going to delete: " + item.name)
             # self.tester.delete_as_group(names=item.name, force=True)
