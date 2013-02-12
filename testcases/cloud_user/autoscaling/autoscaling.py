117fdd2bc2399f54533c5d9f04d3971357b27a12
Tony Beckham <tony.beckham@gmail.com>
2013-02-12 09:18:21 -0800

    Checking for any output when delete fails, java sdk gives "Resource already exists"

--- testcases/cloud_user/autoscaling/autoscaling.py
+++ testcases/cloud_user/autoscaling/autoscaling.py
@@ -97,6 +97,7 @@ class AutoScalingBasics(EutesterTestCase):
         # self.tester.delete_as_group("ASG-1360656706.75")
         for item in self.tester.describe_as_group():
             self.debug("Found Auto Scaling Group: " + item.name)
+            self.tester.delete_as_group(item.name, True)
 
 if __name__ == "__main__":
     testcase = AutoScalingBasics()
