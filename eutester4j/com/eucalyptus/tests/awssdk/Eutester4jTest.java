abbe76f7f457fcc1cb7eb15fcdecc417d2807af7
Tony Beckham <tony.beckham@gmail.com>
2013-02-20 15:36:05 -0800

    Add ELB connection to eutester4j and EC2 and ELB validation tests

--- eutester4j/com/eucalyptus/tests/awssdk/Eutester4jTest.java
+++ eutester4j/com/eucalyptus/tests/awssdk/Eutester4jTest.java
@@ -43,9 +43,16 @@ import org.testng.AssertJUnit;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.autoscaling.AmazonAutoScaling;
 import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+import com.amazonaws.services.ec2.model.DescribeInstanceStatusRequest;
+import com.amazonaws.services.ec2.model.DescribeInstanceStatusResult;
+import com.amazonaws.services.ec2.model.InstanceStatus;
 import com.amazonaws.services.ec2.model.SecurityGroup;
 import com.amazonaws.services.autoscaling.model.LaunchConfiguration;
 
@@ -59,29 +66,14 @@ public class Eutester4jTest {
 	
 	private static AmazonEC2 ec2;
 	private static AmazonAutoScaling as;
-	private static String ec2Endpoint = null;
-	private static String asEndpoint = null;
-	private static String secretKey = null;
-	private static String accessKey= null;
-	private static String credpath = null;
-	
+	private static String emi;
 	private static String secGroupName = "TestGroup-" + eucaUUID();
 	private static String secGroupDesc = "TestDesc-" + eucaUUID();
 	private static String keyName = "TestKey-" + eucaUUID();
 	private static ArrayList<String> securityGroups = new ArrayList<String>();
+	
+//	private static String emi = "emi-8A1144D2"; //euca an emi you have access to
 
-	/*
-	 * for testing against euca cloud
-	 */
-//	private static String emi = "emi-52F13945"; //euca an emi you have access to
-//	private static String type = "m1.small"; //euca
-	
-	/*
-	 * for testing against AWS
-	 */
-	private static String type = "t1.micro"; //AWS
-	private static String emi = "ami-921f3fd7"; //AWS an ami you have access to
-	
 	/**
 	 * Called once, before the first test is run. Creates an ec2 connection.
      * 
@@ -89,34 +81,14 @@ public class Eutester4jTest {
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		getCloudInfo();
+		as = getAutoScalingClient(ACCESS_KEY, SECRET_KEY, AS_ENDPOINT);
+		ec2 = getEc2Client(ACCESS_KEY, SECRET_KEY, EC2_ENDPOINT);
+		emi = findImage(ec2);
 		
-		/*
-		 * For testing against Eucacloud
-		 */
-		credpath = "/Users/tony/Desktop/as_test_cloud/eucarc"; //euca
-		secretKey = parseEucarc(credpath, "EC2_SECRET_KEY").replace("'", ""); //euca
-		accessKey = parseEucarc(credpath, "EC2_ACCESS_KEY").replace("'", ""); //euca
-		ec2Endpoint = parseEucarc(credpath, "EC2_URL")+"/"; //euca
-		asEndpoint = parseEucarc(credpath, "AWS_AUTO_SCALING_URL") + "/"; //euca
-		ec2 = getEc2Client(accessKey, secretKey, ec2Endpoint);
-		as = getAutoScalingClient(accessKey, secretKey, asEndpoint);
-//		ec2 = ec2Connection(accessKey,secretKey); //euca
-//		as = asConnection(accessKey, secretKey); //euca
-		
-		/*
-		 * For testing against AWS
-		 */
-//		AWSCredentials credentials = new PropertiesCredentials(
-//                Eutester4j.class.getResourceAsStream("AwsCredentials.properties")); //AWS
-//		ec2 = new AmazonEC2Client(credentials); //AWS
-//		as = new AmazonAutoScalingClient(credentials); //AWS
 //		ec2Endpoint = "http://ec2.us-west-1.amazonaws.com"; //AWS
 //		asEndpoint = "http://autoscaling.us-west-1.amazonaws.com"; //AWS
-//		secretKey = credentials.getAWSSecretKey(); //AWS
-//		accessKey = credentials.getAWSAccessKeyId(); //AWS
-		
-//		ec2.setEndpoint(ec2Endpoint);
-//		as.setEndpoint(asEndpoint);
+
 		createSecurityGoup(ec2, secGroupName, secGroupDesc);
 		createKeyPair(ec2, keyName);
 		securityGroups.add(secGroupName);
@@ -134,43 +106,41 @@ public class Eutester4jTest {
 		deleteSecurityGroup(ec2, secGroupName);
 		ec2 = null;
 		as=null;
-		secretKey = null;
-		accessKey = null;
-		ec2Endpoint = null;
-		asEndpoint = null;
 	}
 
-//	/*
-//	 * Test that invalid access key prevents execution of ec2 commands. There should be an exception caught
-//	 * After the test, credentials are reset to values present before the test ran.
-//	 */
-//	@Test(enabled=true)
-//	public void testInvalidAccessKey() {
-//		ec2Connection("badAccessKey", secretKey);
-//		try {
-//			ec2.describeAvailabilityZones();
-//		} catch (Exception e) {
-//			AssertJUnit.assertTrue(e.getMessage().length() > 0);
-//		} finally {
-//			ec2Connection(accessKey, secretKey);
-//		}
-//	}
-//	
-//	/*
-//	 * Test that invalid secret key prevents execution of ec2 commands. There should be an exception caught.
-//	 * After the test, credentials are reset to values present before the test ran
-//	 */
-//	@Test(enabled=true)
-//	public void testInvalidSecretKey() {
-//		ec2Connection(accessKey, "badSecretKey");
-//		try {
-//			ec2.describeAvailabilityZones();
-//		} catch (Exception e) {
-//			AssertJUnit.assertTrue(e.getMessage().length() > 0);
-//		} finally {
-//			ec2Connection(accessKey, secretKey);
-//		}
-//	}
+	/*
+	 * Test that invalid access key prevents execution of ec2 commands. There should be an exception caught
+	 * After the test, credentials are reset to values present before the test ran.
+	 */
+	@Test(enabled=true)
+	public void testInvalidAccessKey() {
+		AWSCredentials creds = new BasicAWSCredentials("badAccessKey",SECRET_KEY);
+		AmazonEC2 ec2Conn = new AmazonEC2Client(creds);
+		ec2Conn.setEndpoint(EC2_ENDPOINT);
+		try {
+			ec2Conn.describeAvailabilityZones();
+		} catch (Exception e) {
+			System.out.println("Got Expected Failure: " +e.getMessage());
+			AssertJUnit.assertTrue(e.getMessage().length() > 0);
+		} 
+	}
+	
+	/*
+	 * Test that invalid secret key prevents execution of ec2 commands. There should be an exception caught.
+	 * After the test, credentials are reset to values present before the test ran
+	 */
+	@Test(enabled=true)
+	public void testInvalidSecretKey() {
+		AWSCredentials creds = new BasicAWSCredentials(ACCESS_KEY,"badSecretKey");
+		AmazonEC2 ec2Conn = new AmazonEC2Client(creds);
+		ec2Conn.setEndpoint(EC2_ENDPOINT);
+		try {
+			ec2Conn.describeAvailabilityZones();
+		} catch (Exception e) {
+			System.out.println("Got Expected Faiilure: " +e.getMessage());
+			AssertJUnit.assertTrue(e.getMessage().length() > 0);
+		} 
+	}
 	
 	/*
 	 * Test that valid credentials have a successful connection by verifying
@@ -232,7 +202,7 @@ public class Eutester4jTest {
 	@Test(enabled=false)
 	public void testRunInstances(){
 		int initalInstanceCount = getInstancesList(ec2).size();
-		runInstances(ec2, emi, keyName, type, securityGroups, 1, 1);
+		runInstances(ec2, emi, keyName, instanceType, securityGroups, 1, 1);
 		AssertJUnit.assertTrue(getInstancesList(ec2).size() > initalInstanceCount);
 		
 		// terminate after the test
@@ -244,29 +214,34 @@ public class Eutester4jTest {
 
 	}
 	
-//	/**
-//	 * test stopping instances (bfebs image required)
-//	 *
-//	 * appears there is a compatibility issue with euca supporting getState() for instances
-//	 * it is always returning null
-//	 */
-//	@Test(enabled=false)
-//	public void testStopInstances(){
-//		runInstances(ec2, emi, keyName, type, securityGroups, 1, 1);
-//		List<String> instanceIds = new ArrayList<String>();
-//		instanceIds.add(getLastlaunchedInstance(ec2).get(0).getInstanceId());
-//		System.out.println("State Check 1: " + getInstanceStateName(ec2, instanceIds));
-//		while(!getLastlaunchedInstance(ec2).get(0).getState().getName().equals("running")){}
-//		System.out.println("State Check 2: " + getInstanceStateName(ec2, instanceIds));
-//		stopInstances(ec2, instanceIds);
-//		System.out.println("State Check 3: " + getInstanceStateName(ec2, instanceIds));
-//		while(!getLastlaunchedInstance(ec2).get(0).getState().getName().equals("stopped")){}
-//		System.out.println("State Check 4: " + getInstanceStateName(ec2, instanceIds));
-//		AssertJUnit.assertTrue(getLastlaunchedInstance(ec2).get(0).getState().getName().equals("stopped"));
-//		// after test terminate the instance
-//		terminateInstances(ec2, instanceIds);
-//		System.out.println("State Check 6: " + getInstanceStateName(ec2, instanceIds));
-//	}
+	/**
+	 * test stopping instances (bfebs image required)
+	 *
+	 * appears there is a compatibility issue with euca supporting getState() for instances
+	 * it is always returning null
+	 */
+	@Test(enabled=false)
+	public void testStopInstances(){
+		runInstances(ec2, emi, keyName, instanceType, securityGroups, 1, 1);
+		List<String> instanceIds = new ArrayList<String>();
+		instanceIds.add(getLastlaunchedInstance(ec2).get(0).getInstanceId());
+		
+		String instance = getLastlaunchedInstance(ec2).get(0).getInstanceId();
+		
+		final DescribeInstanceStatusResult instanceStatusResult = ec2
+				.describeInstanceStatus(new DescribeInstanceStatusRequest()
+						.withInstanceIds(instance));
+		
+		final InstanceStatus status = instanceStatusResult
+				.getInstanceStatuses().get(0);
+		System.out.println("Status: " + status.getInstanceState().toString());
+		while(!getLastlaunchedInstance(ec2).get(0).getState().getName().equals("running")){}
+		stopInstances(ec2, instanceIds);
+		while(!getLastlaunchedInstance(ec2).get(0).getState().getName().equals("stopped")){}
+		AssertJUnit.assertTrue(getLastlaunchedInstance(ec2).get(0).getState().getName().equals("stopped"));
+		// after test terminate the instance
+		terminateInstances(ec2, instanceIds);
+	}
 	
 	/**
 	 * test terminating
@@ -276,7 +251,7 @@ public class Eutester4jTest {
 	 */
 	@Test(enabled=false)
 	public void testTerminateInstances(){
-		runInstances(ec2, emi, keyName, type, securityGroups, 1, 1);
+		runInstances(ec2, emi, keyName, instanceType, securityGroups, 1, 1);
 		List<String> instanceIds = new ArrayList<String>();
 		instanceIds.add(getLastlaunchedInstance(ec2).get(0).getInstanceId());
 		System.out.println("Created instance = " + getLastlaunchedInstance(ec2).get(0).getInstanceId());
@@ -307,7 +282,7 @@ public class Eutester4jTest {
 	public void testBasicLaunchConfig(){
 		String launchConfigurationName = "LC-" + eucaUUID();
 		try {
-			createLaunchConfig(as, launchConfigurationName, emi, type, keyName, securityGroups);
+			createLaunchConfig(as, launchConfigurationName, emi, instanceType, keyName, securityGroups);
 			
 			List<LaunchConfiguration> launchConfigs = describeLaunchConfigs(as);
 			
---
+++ eutester4j/com/eucalyptus/tests/awssdk/TestAutoScalingELBReferenceValidation.java
@@ -0,0 +1,148 @@
+/*************************************************************************
+ * Copyright 2009-2013 Eucalyptus Systems, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
+ * CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
+ * additional information or have any questions.
+ ************************************************************************/
+package com.eucalyptus.tests.awssdk;
+
+import static com.eucalyptus.tests.awssdk.Eutester4j.*;
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.autoscaling.AmazonAutoScaling;
+import com.amazonaws.services.autoscaling.model.CreateAutoScalingGroupRequest;
+import com.amazonaws.services.autoscaling.model.CreateLaunchConfigurationRequest;
+import com.amazonaws.services.autoscaling.model.DeleteAutoScalingGroupRequest;
+import com.amazonaws.services.autoscaling.model.DeleteLaunchConfigurationRequest;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancing;
+import com.amazonaws.services.elasticloadbalancing.model.CreateLoadBalancerRequest;
+import com.amazonaws.services.elasticloadbalancing.model.DeleteLoadBalancerRequest;
+import com.amazonaws.services.elasticloadbalancing.model.Listener;
+
+/**
+ * This application tests reference validation for auto scaling groups with ELB.
+ * 
+ * This is verification for the story:
+ * 
+ * https://eucalyptus.atlassian.net/browse/EUCA-5027
+ */
+public class TestAutoScalingELBReferenceValidation {
+
+	@Test
+	public void test() throws Exception {
+		getCloudInfo();
+		final AmazonAutoScaling as = getAutoScalingClient(ACCESS_KEY,
+				SECRET_KEY, AS_ENDPOINT);
+		final AmazonEC2 ec2 = getEc2Client(ACCESS_KEY, SECRET_KEY, EC2_ENDPOINT);
+		final AmazonElasticLoadBalancing elb = getElbClient(ACCESS_KEY,
+				SECRET_KEY, ELB_ENDPOINT);
+		final String imageId = findImage(ec2);
+		final String availabilityZone = findAvalablityZone(ec2);
+		final String namePrefix = eucaUUID() + "-";
+		print("Using resource prefix for test: " + namePrefix);
+
+		final List<Runnable> cleanupTasks = new ArrayList<Runnable>();
+		try {
+			// Generate a load balancer to use
+			final String loadBalancerName = namePrefix + "ELBReferenceTest";
+			print("Creating a load balancer for test use: " + loadBalancerName);
+			elb.createLoadBalancer(new CreateLoadBalancerRequest()
+					.withLoadBalancerName(loadBalancerName)
+					.withAvailabilityZones(availabilityZone)
+					.withListeners(
+							new Listener().withInstancePort(8888)
+									.withLoadBalancerPort(8888)
+									.withProtocol("HTTP")));
+			cleanupTasks.add(new Runnable() {
+				@Override
+				public void run() {
+					print("Deleting load balancer: " + loadBalancerName);
+					elb.deleteLoadBalancer(new DeleteLoadBalancerRequest()
+							.withLoadBalancerName(loadBalancerName));
+				}
+			});
+
+			// Register cleanup for launch config
+			final String configName = namePrefix + "ELBReferenceTest";
+			cleanupTasks.add(new Runnable() {
+				@Override
+				public void run() {
+					print("Deleting launch configuration: " + configName);
+					as.deleteLaunchConfiguration(new DeleteLaunchConfigurationRequest()
+							.withLaunchConfigurationName(configName));
+				}
+			});
+
+			// Create launch configuration
+			print("Creating launch configuration: " + configName);
+			as.createLaunchConfiguration(new CreateLaunchConfigurationRequest()
+					.withLaunchConfigurationName(configName)
+					.withImageId(imageId).withInstanceType(instanceType));
+
+			// Register cleanup for auto scaling group
+			final String groupName = namePrefix + "ELBReferenceTest";
+			cleanupTasks.add(new Runnable() {
+				@Override
+				public void run() {
+					print("Deleting group: " + groupName);
+					as.deleteAutoScalingGroup(new DeleteAutoScalingGroupRequest()
+							.withAutoScalingGroupName(groupName)
+							.withForceDelete(true));
+				}
+			});
+
+			// Create scaling group with invalid availability zone
+			print("Creating auto scaling group with invalid load balancer: "
+					+ groupName);
+			try {
+				as.createAutoScalingGroup(new CreateAutoScalingGroupRequest()
+						.withAutoScalingGroupName(groupName)
+						.withLaunchConfigurationName(configName).withMinSize(0)
+						.withMaxSize(2).withAvailabilityZones(availabilityZone)
+						.withLoadBalancerNames("invalid load balancer name"));
+				assertThat(false, "Creation should fail");
+			} catch (AmazonServiceException e) {
+				print("Expected error returned: " + e);
+			}
+
+			// Create scaling group
+			print("Creating auto scaling group: " + groupName);
+			as.createAutoScalingGroup(new CreateAutoScalingGroupRequest()
+					.withAutoScalingGroupName(groupName)
+					.withLaunchConfigurationName(configName).withMinSize(0)
+					.withMaxSize(1).withAvailabilityZones(availabilityZone)
+					.withLoadBalancerNames(loadBalancerName));
+
+			print("Test complete");
+		} finally {
+			// Attempt to clean up anything we created
+			Collections.reverse(cleanupTasks);
+			for (final Runnable cleanupTask : cleanupTasks) {
+				try {
+					cleanupTask.run();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
