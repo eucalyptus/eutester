750241935dde752df5ea2b54a0810e0973a0a4a3
Vic Iglesias <viglesiasce@gmail.com>
2013-02-26 11:55:04 -0800

    Add statistics checks to cloud watch tests:
    Maximum
    Minimum
    Average
    Sum
    SampleCount

--- testcases/cloud_user/cloudwatch/cloudwatchbasics.py
+++ testcases/cloud_user/cloudwatch/cloudwatchbasics.py
@@ -4,8 +4,7 @@ import time
 from eucaops import Eucaops
 from eucaops import CWops
 from eutester.eutestcase import EutesterTestCase
-import os
-import random
+from boto.ec2.cloudwatch import Metric
 import datetime
 
 class CloudWatchBasics(EutesterTestCase):
@@ -21,50 +20,16 @@ class CloudWatchBasics(EutesterTestCase):
             self.tester = CWops( credpath=self.args.credpath, region=self.args.region)
         else:
             self.tester = Eucaops(config_file=self.args.config, password=self.args.password, credpath=self.args.credpath)
-        self.tester.poll_count = 120
-
-        ### Add and authorize a group for the instance
-        self.group = self.tester.add_group(group_name="group-" + str(time.time()))
-        self.tester.authorize_group_by_name(group_name=self.group.name )
-        self.tester.authorize_group_by_name(group_name=self.group.name, port=-1, protocol="icmp" )
-        ### Generate a keypair for the instance
-        self.keypair = self.tester.add_keypair( "keypair-" + str(time.time()))
-        self.keypath = '%s/%s.pem' % (os.curdir, self.keypair.name)
-        self.image = self.args.emi
-        if not self.image:
-            self.image = self.tester.get_emi(root_device_type="instance-store")
-        self.address = None
-        self.volume = None
-        self.snapshot = None
-        self.private_addressing = False
-        zones = self.tester.ec2.get_all_zones()
-        self.zone = random.choice(zones).name
-        self.reservation = None
+        self.start_time =  str(int(time.time()))
+        self.namespace = "Namespace-" + self.start_time
 
     def clean_method(self):
-        ### Terminate the reservation if it is still up
-        if self.reservation:
-            for instance in self.reservation.instances:
-                instance.delete_tags(instance.tags)
-            self.assertTrue(self.tester.terminate_instances(self.reservation), "Unable to terminate instance(s)")
+        pass
 
-        if self.volume:
-            self.tester.delete_volume(self.volume,timeout=600)
-
-        if self.snapshot:
-            self.tester.delete_snapshot(self.snapshot)
-
-        ### DELETE group
-        self.tester.delete_group(self.group)
-
-        ### Delete keypair in cloud and from filesystem
-        self.tester.delete_keypair(self.keypair)
-        os.remove(self.keypath)
-
-    def get_time_window(self, hours_ago=1, end=None):
+    def get_time_window(self, end=None, **kwargs):
         if not end:
             end = datetime.datetime.utcnow()
-        start = end - datetime.timedelta(hours=hours_ago)
+        start = end - datetime.timedelta(**kwargs)
         return (start,end)
 
     def print_timeseries_for_graphite(timeseries):
@@ -72,28 +37,61 @@ class CloudWatchBasics(EutesterTestCase):
                 print "graph.Namespace-1361426618 " + str(int(datapoint['Average'])) + " " + \
                       str((datapoint['Timestamp'] - datetime.datetime(1970,1,1)).total_seconds())
 
-    def GetPut(self):
-        seconds_to_put_data = 600000000000000
-        starting_metric_data = 1
+    def PutDataGetStats(self):
+        seconds_to_put_data = 120
+        metric_data = 1
         time_string =  str(int(time.time()))
-        namespace = "Namespace-" + time_string
         metric_name = "Metric-" + time_string
         incrementing = True
+        while datetime.datetime.now().second != 0:
+            self.tester.debug("Waiting for minute edge")
+            self.tester.sleep(1)
+        start = datetime.datetime.utcnow()
         for i in xrange(seconds_to_put_data):
-            self.tester.cw.put_metric_data(namespace, [metric_name],[starting_metric_data])
-            if starting_metric_data == 600 or starting_metric_data == 0:
+            self.tester.debug("Adding metric: {metric} to namescace: {namespace} with value {value}".format(
+                metric=metric_name, namespace = self.namespace, value=metric_data))
+            self.tester.cw.put_metric_data(self.namespace, [metric_name],[metric_data])
+            if metric_data == 600 or metric_data == 0:
                 incrementing = not incrementing
             if incrementing:
-                starting_metric_data += 1
+                metric_data += 1
             else:
-                starting_metric_data -= 1
+                metric_data -= 1
             self.tester.sleep(1)
+        end = start + datetime.timedelta(minutes=2)
+        metric = self.tester.cw.list_metrics(namespace=self.namespace)[0]
+        assert isinstance(metric,Metric)
+        stats_array = metric.query(start_time=start, end_time=end, statistics=['Average', 'Sum', 'Maximum', 'Minimum','SampleCount'] )
+        assert len(stats_array) == 2
+        if stats_array[0]['Minimum'] == 1:
+            first_sample = stats_array[0]
+            second_sample = stats_array[1]
+        else:
+            second_sample = stats_array[0]
+            first_sample = stats_array[1]
+        print stats_array
+
+        ##Check sample 1
+        assert first_sample['Maximum'] < 60 and first_sample['Minimum'] > 0
+        assert first_sample['Average'] < 34 and first_sample['Average'] > 26
+        assert first_sample['Sum'] < 1650 and first_sample['Sum'] > 1550
+        assert first_sample['SampleCount'] > 50
+        ##Check sample 2
+        assert second_sample['Maximum'] < 120 and second_sample['Minimum'] > 50
+        assert second_sample['Average'] < 90 and second_sample['Average'] > 80
+        assert second_sample['Sum'] < 4900 and second_sample['Sum'] > 4600
+        assert second_sample['SampleCount'] > 50
+
+        assert first_sample['Average'] < second_sample['Average']
+        assert first_sample['Sum'] < second_sample['Sum']
+        assert first_sample['Maximum'] < second_sample['Maximum']
+        assert first_sample['Minimum'] < second_sample['Minimum']
 
 if __name__ == "__main__":
     testcase = CloudWatchBasics()
     ### Use the list of tests passed from config/command line to determine what subset of tests to run
     ### or use a predefined list  "VolumeTagging", "InstanceTagging", "SnapshotTagging", "ImageTagging"
-    list = testcase.args.tests or ["GetPut"]
+    list = testcase.args.tests or ["PutDataGetStats"]
 
     ### Convert test suite methods to EutesterUnitTest objects
     unit_list = [ ]
