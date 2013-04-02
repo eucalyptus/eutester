6e87e55ad8246e90c64dbeb2768c9aeeda61d34f
Tony Beckham <tony.beckham@gmail.com>
2013-04-02 09:08:39 -0700

    Add reaming tests from AS backlog.

---
+++ eutester4j/com/eucalyptus/tests/awssdk/TestAutoScalingMetricsManagement.java
@@ -0,0 +1,187 @@
+/*************************************************************************
+ * Copyright 2009-2013 Eucalyptus Systems, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
+ * CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
+ * additional information or have any questions.
+ ************************************************************************/
+package com.eucalyptus.tests.awssdk;
+
+import com.amazonaws.services.autoscaling.model.*;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.eucalyptus.tests.awssdk.Eutester4j.*;
+
+
+/**
+ * This application tests metrics management for auto scaling groups.
+ * <p/>
+ * This is verification for the story:
+ * <p/>
+ * https://eucalyptus.atlassian.net/browse/EUCA-5354
+ */
+public class TestAutoScalingMetricsManagement {
+    @Test
+    public void AutoScalingMetricsManagementTest() throws Exception {
+        testInfo(this.getClass().getSimpleName());
+        getCloudInfo();
+        final List<Runnable> cleanupTasks = new ArrayList<Runnable>();
+        try {
+            // Register cleanup for launch configs
+            final String configName = NAME_PREFIX + "MetricsTest";
+            cleanupTasks.add(new Runnable() {
+                @Override
+                public void run() {
+                    print("Deleting launch configuration: " + configName);
+                    as.deleteLaunchConfiguration(new DeleteLaunchConfigurationRequest().withLaunchConfigurationName(configName));
+                }
+            });
+
+            // Create launch configuration
+            print("Creating launch configuration: " + configName);
+            as.createLaunchConfiguration(new CreateLaunchConfigurationRequest()
+                    .withLaunchConfigurationName(configName)
+                    .withImageId(IMAGE_ID)
+                    .withInstanceType(INSTANCE_TYPE));
+
+            // Register cleanup for auto scaling groups
+            final String groupName = NAME_PREFIX + "MetricsTest";
+            cleanupTasks.add(new Runnable() {
+                @Override
+                public void run() {
+                    print("Deleting group: " + groupName);
+                    as.deleteAutoScalingGroup(new DeleteAutoScalingGroupRequest().withAutoScalingGroupName(groupName).withForceDelete(true));
+                }
+            });
+
+            // Create scaling group
+            print("Creating auto scaling group: " + groupName);
+            as.createAutoScalingGroup(new CreateAutoScalingGroupRequest()
+                    .withAutoScalingGroupName(groupName)
+                    .withLaunchConfigurationName(configName)
+                    .withMinSize(0)
+                    .withMaxSize(0)
+                    .withAvailabilityZones(AVAILABILITY_ZONE));
+
+            // Describe metrics collection types
+            print("Describeing metric collection types");
+            final DescribeMetricCollectionTypesResult collectionTypesResult = as.describeMetricCollectionTypes();
+            print(collectionTypesResult.toString());
+            assertThat(collectionTypesResult.getGranularities() != null, "Expected granularities");
+            assertThat(collectionTypesResult.getGranularities().size() == 1, "Expected one granularity");
+            assertThat("1Minute".equals(collectionTypesResult.getGranularities().get(0).getGranularity()), "Unexpected granularity");
+            assertThat(collectionTypesResult.getMetrics() != null, "Expected granularities");
+            assertThat(collectionTypesResult.getMetrics().size() == 7, "Expected 7 granularities");
+            assertThat("GroupDesiredCapacity".equals(collectionTypesResult.getMetrics().get(0).getMetric()), "Unexpected metric");
+            assertThat("GroupInServiceInstances".equals(collectionTypesResult.getMetrics().get(1).getMetric()), "Unexpected metric");
+            assertThat("GroupMaxSize".equals(collectionTypesResult.getMetrics().get(2).getMetric()), "Unexpected metric");
+            assertThat("GroupMinSize".equals(collectionTypesResult.getMetrics().get(3).getMetric()), "Unexpected metric");
+            assertThat("GroupPendingInstances".equals(collectionTypesResult.getMetrics().get(4).getMetric()), "Unexpected metric");
+            assertThat("GroupTerminatingInstances".equals(collectionTypesResult.getMetrics().get(5).getMetric()), "Unexpected metric");
+            assertThat("GroupTotalInstances".equals(collectionTypesResult.getMetrics().get(6).getMetric()), "Unexpected metric");
+
+            // Describe group to check no metrics enabled
+            print("Checking no enabled metrics for group: " + groupName);
+            {
+                final DescribeAutoScalingGroupsResult groupResult = as.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames(groupName));
+                assertThat(groupResult.getAutoScalingGroups() != null, "Expected groups");
+                assertThat(groupResult.getAutoScalingGroups().size() == 1, "Expected 1 group");
+                assertThat(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics() == null ||
+                        groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().isEmpty(), "Expected no metrics enabled.");
+            }
+
+            // Enable all metrics
+            print("Enabling all metrics for group: " + groupName);
+            as.enableMetricsCollection(new EnableMetricsCollectionRequest().withAutoScalingGroupName(groupName).withGranularity("1Minute"));
+
+            // Describe group to check metrics enabled
+            print("Checking all metrics enabled for group: " + groupName);
+            {
+                final DescribeAutoScalingGroupsResult groupResult = as.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames(groupName));
+                assertThat(groupResult.getAutoScalingGroups() != null, "Expected groups");
+                assertThat(groupResult.getAutoScalingGroups().size() == 1, "Expected 1 group");
+                assertThat(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics() != null &&
+                        groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().size() == 7, "Expected 1 metric enabled.");
+                assertThat("GroupDesiredCapacity".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(0).getMetric()), "Unexpected metric");
+                assertThat("GroupInServiceInstances".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(1).getMetric()), "Unexpected metric");
+                assertThat("GroupMaxSize".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(2).getMetric()), "Unexpected metric");
+                assertThat("GroupMinSize".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(3).getMetric()), "Unexpected metric");
+                assertThat("GroupPendingInstances".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(4).getMetric()), "Unexpected metric");
+                assertThat("GroupTerminatingInstances".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(5).getMetric()), "Unexpected metric");
+                assertThat("GroupTotalInstances".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(6).getMetric()), "Unexpected metric");
+            }
+
+            // Disable all metrics
+            print("Disabling all metrics for group: " + groupName);
+            as.disableMetricsCollection(new DisableMetricsCollectionRequest().withAutoScalingGroupName(groupName));
+
+            // Describe group to check no metrics enabled
+            print("Checking no enabled metrics for group: " + groupName);
+            {
+                final DescribeAutoScalingGroupsResult groupResult = as.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames(groupName));
+                assertThat(groupResult.getAutoScalingGroups() != null, "Expected groups");
+                assertThat(groupResult.getAutoScalingGroups().size() == 1, "Expected 1 group");
+                assertThat(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics() == null ||
+                        groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().isEmpty(), "Expected no metrics enabled.");
+            }
+
+            // Enable single metric
+            print("Enabling GroupInServiceInstances metric for group: " + groupName);
+            as.enableMetricsCollection(new EnableMetricsCollectionRequest().withAutoScalingGroupName(groupName).withMetrics("GroupInServiceInstances").withGranularity("1Minute"));
+
+            // Describe group to check single enabled
+            print("Checking GroupInServiceInstances metric enabled for group: " + groupName);
+            {
+                final DescribeAutoScalingGroupsResult groupResult = as.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames(groupName));
+                assertThat(groupResult.getAutoScalingGroups() != null, "Expected groups");
+                assertThat(groupResult.getAutoScalingGroups().size() == 1, "Expected 1 group");
+                assertThat(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics() != null &&
+                        groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().size() == 1, "Expected 1 metric enabled.");
+                assertThat("GroupInServiceInstances".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(0).getMetric()), "Expected 1 metric enabled.");
+                assertThat("1Minute".equals(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().get(0).getGranularity()), "Expected granularity for enabled metric.");
+            }
+
+            // Disable single metric
+            print("Disabling GroupInServiceInstances metric for group: " + groupName);
+            as.disableMetricsCollection(new DisableMetricsCollectionRequest().withAutoScalingGroupName(groupName).withMetrics("GroupInServiceInstances"));
+
+            // Describe group to check no metrics enabled
+            print("Checking no enabled metrics for group: " + groupName);
+            {
+                final DescribeAutoScalingGroupsResult groupResult = as.describeAutoScalingGroups(new DescribeAutoScalingGroupsRequest().withAutoScalingGroupNames(groupName));
+                assertThat(groupResult.getAutoScalingGroups() != null, "Expected groups");
+                assertThat(groupResult.getAutoScalingGroups().size() == 1, "Expected 1 group");
+                assertThat(groupResult.getAutoScalingGroups().get(0).getEnabledMetrics() == null ||
+                        groupResult.getAutoScalingGroups().get(0).getEnabledMetrics().isEmpty(), "Expected no metrics enabled.");
+            }
+
+            print("Test complete");
+        } finally {
+            // Attempt to clean up anything we created
+            Collections.reverse(cleanupTasks);
+            for (final Runnable cleanupTask : cleanupTasks) {
+                try {
+                    cleanupTask.run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ eutester4j/com/eucalyptus/tests/awssdk/TestEC2InstanceProfile.java
@@ -0,0 +1,193 @@
+/*************************************************************************
+ * Copyright 2009-2013 Eucalyptus Systems, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
+ * CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
+ * additional information or have any questions.
+ ************************************************************************/
+package com.eucalyptus.tests.awssdk;
+
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.*;
+import com.amazonaws.services.identitymanagement.model.CreateInstanceProfileRequest;
+import com.amazonaws.services.identitymanagement.model.CreateInstanceProfileResult;
+import com.amazonaws.services.identitymanagement.model.DeleteInstanceProfileRequest;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.eucalyptus.tests.awssdk.Eutester4j.*;
+
+/**
+ * This application test the EC2 RunInstances operation with instance profiles.
+ * <p/>
+ * This is verification for the task:
+ * <p/>
+ * https://eucalyptus.atlassian.net/browse/EUCA-5407
+ */
+public class TestEC2InstanceProfile {
+
+    @Test
+    public void EC2InstanceProfileTest() throws Exception {
+        testInfo(this.getClass().getSimpleName());
+        getCloudInfo();
+
+        // End discovery, start test
+        final List<Runnable> cleanupTasks = new ArrayList<Runnable>();
+        try {
+            final String namePrefix = UUID.randomUUID().toString() + "-";
+            print("Using prefix for test: " + namePrefix);
+
+            // Create instance profile
+            final String profileName = namePrefix + "EC2ProfileTest";
+            print("Creating instance profile: " + profileName);
+            final CreateInstanceProfileResult instanceProfileResult = iam.createInstanceProfile(new CreateInstanceProfileRequest()
+                    .withInstanceProfileName(profileName)
+                    .withPath("/path"));
+            cleanupTasks.add(new Runnable() {
+                @Override
+                public void run() {
+                    print("Deleting instance profile: " + profileName);
+                    iam.deleteInstanceProfile(new DeleteInstanceProfileRequest()
+                            .withInstanceProfileName(profileName));
+                }
+            });
+            final String profileArn = instanceProfileResult.getInstanceProfile().getArn();
+            print("Created instance profile with ARN: " + profileArn);
+
+            // Run instance with ARN
+            {
+                print("Running instance with instance profile ARN");
+                final RunInstancesResult runResult =
+                        ec2.runInstances(new RunInstancesRequest()
+                                .withImageId(IMAGE_ID)
+                                .withMinCount(1)
+                                .withMaxCount(1)
+                                .withIamInstanceProfile(new IamInstanceProfileSpecification()
+                                        .withArn(profileArn)));
+                final String instanceId = getInstancesIds(runResult.getReservation()).get(0);
+                print("Launched instance: " + instanceId);
+                cleanupTasks.add(new Runnable() {
+                    @Override
+                    public void run() {
+                        print("Terminating instance: " + instanceId);
+                        ec2.terminateInstances(new TerminateInstancesRequest().withInstanceIds(instanceId));
+                    }
+                });
+
+                // Wait for instance
+                waitForInstance(ec2, instanceId, "running");
+
+                // Verify instance profile used for instance
+                print("Verifying run instances response references instance profile");
+                assertThat(runResult.getReservation().getInstances().get(0).getIamInstanceProfile() != null, "Expected instance profile");
+                assertThat(profileArn.equals(runResult.getReservation().getInstances().get(0).getIamInstanceProfile().getArn()), "Unexpected instance profile ARN: " + runResult.getReservation().getInstances().get(0).getIamInstanceProfile().getArn());
+                assertThat(runResult.getReservation().getInstances().get(0).getIamInstanceProfile().getId() != null, "Expected instance profile ID");
+
+                //
+                print("Terminating instance: " + instanceId);
+                ec2.terminateInstances(new TerminateInstancesRequest().withInstanceIds(instanceId));
+            }
+
+            // Run instance with name
+            {
+                print("Running instance with instance profile name");
+                final RunInstancesResult runResult =
+                        ec2.runInstances(new RunInstancesRequest()
+                                .withImageId(IMAGE_ID)
+                                .withMinCount(1)
+                                .withMaxCount(1)
+                                .withIamInstanceProfile(new IamInstanceProfileSpecification()
+                                        .withName(profileName)));
+                final String instanceId = getInstancesIds(runResult.getReservation()).get(0);
+                print("Launched instance: " + instanceId);
+                cleanupTasks.add(new Runnable() {
+                    @Override
+                    public void run() {
+                        print("Terminating instance: " + instanceId);
+                        ec2.terminateInstances(new TerminateInstancesRequest().withInstanceIds(instanceId));
+                    }
+                });
+
+                // Wait for instance
+                waitForInstance(ec2, instanceId, "running");
+
+                // Verify instance profile used for instance
+                print("Verifying run instances response references instance profile");
+                assertThat(runResult.getReservation().getInstances().get(0).getIamInstanceProfile() != null, "Expected instance profile");
+                assertThat(profileArn.equals(runResult.getReservation().getInstances().get(0).getIamInstanceProfile().getArn()), "Unexpected instance profile ARN: " + runResult.getReservation().getInstances().get(0).getIamInstanceProfile().getArn());
+                assertThat(runResult.getReservation().getInstances().get(0).getIamInstanceProfile().getId() != null, "Expected instance profile ID");
+
+                //
+                print("Terminating instance: " + instanceId);
+                ec2.terminateInstances(new TerminateInstancesRequest().withInstanceIds(instanceId));
+            }
+
+            print("Test complete");
+        } finally {
+            // Attempt to clean up anything we created
+            Collections.reverse(cleanupTasks);
+            for (final Runnable cleanupTask : cleanupTasks) {
+                try {
+                    cleanupTask.run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String waitForInstance(final AmazonEC2 ec2,
+                                   final String expectedId,
+                                   final String state) throws Exception {
+        print("Waiting for instance state " + state);
+        String az = null;
+        final long startTime = System.currentTimeMillis();
+        boolean completed = false;
+        while (!completed && (System.currentTimeMillis() - startTime) < TimeUnit.MINUTES.toMillis(3)) {
+            final DescribeInstanceStatusResult instanceStatusResult =
+                    ec2.describeInstanceStatus(new DescribeInstanceStatusRequest()
+                            .withInstanceIds(expectedId)
+                            .withIncludeAllInstances(true)
+                            .withFilters(new Filter()
+                                    .withName("instance-state-name")
+                                    .withValues(state)));
+            completed = instanceStatusResult.getInstanceStatuses().size() == 1;
+            if (completed) {
+                az = instanceStatusResult.getInstanceStatuses().get(0).getAvailabilityZone();
+                assertThat(expectedId.equals(instanceStatusResult.getInstanceStatuses().get(0).getInstanceId()), "Incorrect instance id");
+                assertThat(state.equals(instanceStatusResult.getInstanceStatuses().get(0).getInstanceState().getName()), "Incorrect instance state");
+            }
+            Thread.sleep(5000);
+        }
+        assertThat(completed, "Instance not reported within the expected timeout");
+        print("Instance reported " + state + " in " + (System.currentTimeMillis() - startTime) + "ms");
+        return az;
+    }
+
+    private List<String> getInstancesIds(final Reservation... reservations) {
+        final List<String> instances = new ArrayList<String>();
+        for (final Reservation reservation : reservations) {
+            for (final Instance instance : reservation.getInstances()) {
+                instances.add(instance.getInstanceId());
+            }
+        }
+        return instances;
+    }
+}
