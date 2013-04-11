529194467b4b7f523346f35fc53e22d8fd1f23c9
Matt Clark <mclark@eucalyptus.com>
2013-04-10 23:51:59 -0700

    Remove unused filter

--- eutester/eulogger.py
+++ eutester/eulogger.py
@@ -170,8 +170,6 @@ class Eulogger(object):
         file_handler.setLevel(level)
         file_handler.addFilter(Mute_Filter())
 
-
-
     def get_parent_logger_files(self):
         files= []
         for h in self.parent_logger.handlers:
@@ -201,29 +199,5 @@ class Mute_Filter(logging.Filter):
         return False
 
 
-#Another filter for unique single messages w multiple child loggers...
-class Unique(logging.Filter):
-    """Messages are allowed through just once.
-    The 'message' includes substitutions, but is not formatted by the
-    handler. If it were, then practically all messages would be unique!
-    """
-    def __init__(self, name=""):
-        logging.Filter.__init__(self, name)
-        self.reset()
-    def reset(self):
-        """Act as if nothing has happened."""
-        self.__logged = {}
-    def filter(self, rec):
-        """logging.Filter.filter performs an extra filter on the name."""
-        return logging.Filter.filter(self, rec) and self.__is_first_time(rec)
-    def __is_first_time(self, rec):
-        """Emit a message only once."""
-        msg = rec.msg %(rec.args)
-        if msg in self.__logged:
-            self.__logged[msg] += 1
-            return False
-        else:
-            self.__logged[msg] = 1
-            return True
 
  
