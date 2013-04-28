994c1be5778b470712c9d0cd6387eccc55f0e86c
Vic Iglesias <viglesiasce@gmail.com>
2013-04-28 12:36:25 -0700

    More foramtting in eutestcase

--- eutester/eutestcase.py
+++ eutester/eutestcase.py
@@ -947,10 +947,9 @@ class EutesterTestCase(unittest.TestCase):
             buf += self.resultdefault("\n"+ self.getline(80)+"\n", printout=False)
             #Ascii mark up errors using pmethod() so errors are in bold/red, etc...
             pmethod = self.resultfail if not testunit.result == EutesterTestResult.passed else self.resultdefault
-            test_summary_line = str(" ").ljust(20) \
-                                + str("| RESULT: " + str(testunit.result)).ljust(20) + "\n" \
-                                + "| TEST NAME: " + str(testunit.name) + "\n" \
-                                + str(" ").ljust(20) + str("| TIME : " + str(testunit.time_to_run))
+            test_summary_line = str(" ").ljust(20) + str("| RESULT: " + str(testunit.result)).ljust(20) + "\n" +\
+                                str(" ").ljust(20) + "| TEST NAME: " + str(testunit.name) + "\n" + \
+                                str(" ").ljust(20) + str("| TIME : " + str(testunit.time_to_run))
 
 
             buf += pmethod(str(test_summary_line),printout=False)
