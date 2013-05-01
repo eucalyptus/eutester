071cb8536e10ff10116307f1759f9d5d02ae2e4c
Matt Clark <mclark@eucalyptus.com>
2013-04-30 22:21:40 -0700

    Update get node, and node's instance list method

--- eutester/euservice.py
+++ eutester/euservice.py
@@ -613,10 +613,11 @@ class EuserviceManager(object):
         #to avoid update() loop allow enabled_clc to be provided as arg
         clc = enabled_clc or self.get_enabled_clc()
         nodes_strings = clc.machine.sys(self.eucaprefix + \
-                                        "/usr/sbin/euca_conf --list-nodes 2>1 | grep -v warning | grep '^NODE'")
+                                        "/usr/sbin/euca_conf --list-nodes 2>/dev/null | grep '^NODE'")
 
         for node_string in nodes_strings:
             #handle/skip any blank lines first...
+            self.debug('Handling Node string:' + str(node_string))
             node_string = node_string.strip()
             if not node_string:
                 continue
