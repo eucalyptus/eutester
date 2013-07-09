62daffa4730e2f594b875ce42597dd4a55dc2005
Vic Iglesias <viglesiasce@gmail.com>
2013-07-09 11:27:51 -0700

    Fix private ip test logic.

--- testcases/cloud_user/instances/instancetest.py
+++ testcases/cloud_user/instances/instancetest.py
@@ -338,7 +338,7 @@ class InstanceBasics(EutesterTestCase):
             instance.update()
             self.assertFalse(self.tester.ping(instance.ip_address), "Was able to ping instance that should have only had a private IP")
             address.release()
-            if instance.ip_address != "0.0.0.0" or instance.ip_address != instance.private_ip_address:
+            if instance.ip_address != "0.0.0.0" and instance.ip_address != instance.private_ip_address:
                 self.fail("Instance received a new public IP: " + instance.ip_address)
         self.set_reservation(None)
         return reservation
