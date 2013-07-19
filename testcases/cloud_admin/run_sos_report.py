7f132caafbd8820e9cec80df25057c2954da029a
Vic Iglesias <viglesiasce@gmail.com>
2013-07-19 00:32:35 -0700

    Allow ticket number to be passed

--- testcases/cloud_admin/run_sos_report.py
+++ testcases/cloud_admin/run_sos_report.py
@@ -11,11 +11,13 @@ class SampleTest(EutesterTestCase):
     def __init__(self):
         self.setuptestcase()
         self.setup_parser()
-        self.start_time = self.ticket_number = int(time.time())
-        self.parser.add_argument("--remote-dir", default="/root/euca-sosreport-" + str(self.start_time) + "/")
+        self.start_time = int(time.time())
+        self.parser.add_argument("--ticket-number", default=self.start_time)
+        self.parser.add_argument("--remote-dir", default="/root/")
         self.parser.add_argument("--local-dir", default=os.getcwd())
         self.parser.add_argument("--package-url", default="http://mongo.beldurnik.com/RPMS/eucalyptus-sos-plugins-0.1.1-0.el6.noarch.rpm")
         self.get_args()
+        self.remote_dir = self.args.remote_dir + "/euca-sosreport-" + self.args.ticket_number + "/"
         # Setup basic eutester object
         self.tester = Eucaops( config_file=self.args.config,password=self.args.password)
 
@@ -35,12 +37,12 @@ class SampleTest(EutesterTestCase):
         for machine in self.tester.get_component_machines():
             assert isinstance(machine, Machine)
             machine.sys("mkdir -p " + self.args.remote_dir)
-            machine.sys("sosreport --batch --tmp-dir " + self.args.remote_dir + " --ticket-number " + str(self.ticket_number),code=0)
+            machine.sys("sosreport --batch --tmp-dir " + self.args.remote_dir + " --ticket-number " + str(self.args.ticket_number),code=0)
 
     def Download(self):
         for machine in self.tester.get_component_machines():
             assert isinstance(machine, Machine)
-            remote_tarball_path = machine.sys("ls -1 " + self.args.remote_dir + "*" + str(self.ticket_number) + "*.xz", code=0)[0]
+            remote_tarball_path = machine.sys("ls -1 " + self.args.remote_dir + "*" + str(self.args.ticket_number) + "*", code=0)[0]
             tarball = remote_tarball_path.split("/")[-1]
             local_tarball_path = self.args.local_dir + '/' + tarball
             self.tester.debug("Downloading file to: " + local_tarball_path)
