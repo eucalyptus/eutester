8cea28c89b444d778cce7e6f968a13f0478edd88
Alice <alice@eucalyptus.com>
2014-01-28 09:48:04 -0800

    Renamed resource tracker to have addresses instead of ip-addresses .

--- eucaops/ec2ops.py
+++ eucaops/ec2ops.py
@@ -182,7 +182,7 @@ disable_root: false"""
         self.test_resources["keypairs"] = []
         self.test_resources["security-groups"] = []
         self.test_resources["images"] = []
-        self.test_resources["ip-addresses"]=[]
+        self.test_resources["addresses"]=[]
 
 
     def get_ec2_ip(self):
