5c5e8b8a68ae2e51f99a8b1b908b663e8e129d24
Vic Iglesias <viglesiasce@gmail.com>
2014-03-17 22:41:51 -0700

    Fix image virtualization type acquisition
    Change-Id: Ie35aa3c9a692d755a407408f3124fee9c06b3ef2

--- testcases/cloud_user/instances/instancetest.py
+++ testcases/cloud_user/instances/instancetest.py
@@ -51,7 +51,6 @@ class InstanceBasics(EutesterTestCase):
             self.image = emi
         else:
             self.image = self.tester.get_emi(root_device_type="instance-store", not_location="loadbalancer", not_platform="windows")
-        self.image_type = self.tester.ec2.get_all_images([self.image])[0].virtualization_type
         self.address = None
         self.volume = None
         self.private_addressing = False
@@ -99,11 +98,11 @@ class InstanceBasics(EutesterTestCase):
         for instance in reservation.instances:
             self.assertTrue(self.tester.wait_for_reservation(reservation), 'Instance did not go to running')
             self.assertTrue(self.tester.ping(instance.ip_address), 'Could not ping instance')
-            if self.image_type == "paravirtual":
+            if self.image.virtualization_type == "paravirtual":
                 paravirtual_ephemeral = "/dev/" + instance.rootfs_device + "2"
                 self.assertFalse(instance.found("ls -1 " + paravirtual_ephemeral,  "No such file or directory"),
                                  "Did not find ephemeral storage at " + paravirtual_ephemeral)
-            elif self.image_type == "hvm":
+            elif self.image.virtualization_type == "hvm":
                 hvm_ephemeral = "/dev/" + instance.block_device_prefix
                 self.assertFalse(instance.found("ls -1 " + hvm_ephemeral,  "No such file or directory"),
                                  "Did not find ephemeral storage at " + hvm_ephemeral)
@@ -225,7 +224,7 @@ class InstanceBasics(EutesterTestCase):
                             'Incorrect reservation in metadata')
             self.assertTrue(re.match(instance.get_metadata("placement/availability-zone")[0], instance.placement),
                             'Incorrect availability-zone in metadata')
-            if self.image_type == "paravirtual":
+            if self.image.virtualization_type == "paravirtual":
                 self.assertTrue(re.match(instance.get_metadata("kernel-id")[0], instance.kernel),
                                 'Incorrect kernel id in metadata')
                 self.assertTrue(re.match(instance.get_metadata("ramdisk-id")[0], instance.ramdisk),
