583576e9e6b857e20387a5e37c81bf21e5e7180a
Matt Clark <mclark@eucalyptus.com>
2014-03-21 10:47:48 -0700

    Sync fs after creating test file, print dir contents before checking for testfile

--- testcases/cloud_user/instances/bfebstest.py
+++ testcases/cloud_user/instances/bfebstest.py
@@ -119,7 +119,7 @@ class BFEBSBasics(InstanceBasics):
             ## Drop a file so we know if we actually created an image
             current_time = str(int(time.time()))
             temp_file = "/root/my-new-file-" + current_time
-            instance.sys("touch " + temp_file, code=0)
+            instance.sys("touch " + temp_file + " && sync", code=0)
             instance.sys('ls -la', code=0)
             rebooted_image = self.tester.create_image(instance, "BFEBS-test-create-image-reboot-" + current_time)
             instance.connect_to_instance()
@@ -130,6 +130,7 @@ class BFEBSBasics(InstanceBasics):
             new_image_reservation = self.tester.run_instance(**self.run_instance_params)
             for new_instance in new_image_reservation.instances:
                 ## Check that our temp file exists
+                new_instance.sys("ls -la")
                 new_instance.sys("ls " + temp_file, code=0)
             self.tester.terminate_instances(new_image_reservation)
 
@@ -142,7 +143,7 @@ class BFEBSBasics(InstanceBasics):
             ## Drop a file so we know if we actually created an image
             current_time = str(int(time.time()))
             temp_file = "/root/my-new-file-" + current_time
-            instance.sys("touch " + temp_file, code=0)
+            instance.sys("touch " + temp_file + " && sync", code=0)
             instance.sys('ls -la', code=0)
             not_rebooted_image = self.tester.create_image(instance, "BFEBS-test-create-image-noreboot-" + current_time, no_reboot=True)
             ending_uptime = instance.get_uptime()
@@ -152,6 +153,7 @@ class BFEBSBasics(InstanceBasics):
             new_image_reservation = self.tester.run_instance(**self.run_instance_params)
             for new_instance in new_image_reservation.instances:
                 ## Check that our temp file exists
+                new_instance.sys("ls -la")
                 new_instance.sys("ls " + temp_file, code=0)
             self.tester.terminate_instances(new_image_reservation)
 
