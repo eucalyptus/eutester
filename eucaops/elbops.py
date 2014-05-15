786fee62a38e2deda3e8c0ae522c260e3b17bdd2
Tony Beckham <tony.beckham@gmail.com>
2014-05-15 16:57:51 -0700

    Get absolute path from filesystem for location of certificate directory

--- eucaops/elbops.py
+++ eucaops/elbops.py
@@ -307,7 +307,7 @@ class ELBops(Eutester):
         self.debug("removing listener")
         self.elb.delete_load_balancer_listeners(name=lb_name, ports=[port])
 
-    def add_server_cert(self, cert_name, cert_dir= abspath("test_data"), cert_file="ssl_server_certs_basics.crt",
+    def add_server_cert(self, cert_name, cert_dir= abspath("testcases/cloud_user/elb/test_data"), cert_file="ssl_server_certs_basics.crt",
                            key_file="ssl_server_certs_basics.pem"):
         cert_body = open(join(cert_dir, cert_file)).read()
         cert_key = open(join(cert_dir, key_file)).read()
